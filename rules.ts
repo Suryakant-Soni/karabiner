4c16e0728408f7548608c90c5c460c4d30d98083
suryakantsoni1991 <suryakantsoniofficial@gmail.com>
2023-04-16 20:45:12 +0530

    initial for ventura

--- rules.ts
+++ rules.ts
@@ -26,257 +26,170 @@ const rules: KarabinerRules[] = [
         type: "basic",
       },
       {
-        type: "basic",
-        description: "Disable CMD + Tab to force Hyper Key usage",
+        description: "Slash -> Hyper Key",
         from: {
-          key_code: "tab",
-          modifiers: {
-            mandatory: ["left_command"],
-          },
+          key_code: "slash",
         },
         to: [
           {
-            key_code: "tab",
+            key_code: "left_shift",
+            modifiers: [ "left_control", "left_option"],
           },
         ],
+        to_if_alone: [
+          {
+            key_code: "slash",
+          },
+        ],
+        type: "basic",
       },
+      // {
+      //   type: "basic",
+      //   description: "Disable CMD + Tab to force Hyper Key usage",
+      //   from: {
+      //     key_code: "tab",
+      //     modifiers: {
+      //       mandatory: ["left_command"],
+      //     },
+      //   },
+      //   to: [
+      //     {
+      //       key_code: "tab",
+      //     },
+      //   ],
+      // },
     ],
   },
   ...createHyperSubLayers({
     // o = "Open" applications
     o: {
       g: app("Google Chrome"),
-      c: app("Cron"),
+      // c: app("Cron"),
       v: app("Visual Studio Code"),
-      d: app("Discord"),
-      s: app("Slack"),
-      e: app("Superhuman"),
-      n: app("Notion"),
-      t: app("Terminal"),
+      // d: app("Discord"),
+      // s: app("Slack"),
+      // e: app("Superhuman"),
+      // n: app("Notion"),
+      t: app("iTerm"),
       // Open todo list managed via *H*ypersonic
-      h: open(
-        "notion://notion.so/stellatehq/Max-Stoiber-CEO-90ea5326add5408f967278461f37c39b#29b31b030a5a4192b05f3883f7d47fe3"
-      ),
-      z: app("zoom.us"),
-      m: app("Muse"),
-      f: app("Figma"),
-      r: app("Telegram"),
-      // "i"Message
-      i: app("Messages"),
-      p: app("Spotify"),
-      a: app("iA Presenter"),
-      w: open("https://web.whatsapp.com"),
-      l: open("Linear"),
+      // h: open(
+      //   "notion://notion.so/stellatehq/Max-Stoiber-CEO-90ea5326add5408f967278461f37c39b#29b31b030a5a4192b05f3883f7d47fe3"
+      // ),
+      // z: app("zoom.us"),
+      // m: app("Muse"),
+      // f: app("Figma"),
+      // r: app("Telegram"),
+      // // "i"Message
+      // i: app("Messages"),
+      // p: app("Spotify"),
+      // a: app("iA Presenter"),
+      w: app("WhatsApp"),
+      // l: open("Linear"),
     },
-
-    // w = "Window" via rectangle.app
-    w: {
-      semicolon: {
-        description: "Window: Hide",
+    // below is for delete action 
+    k:
+    {
+        h: {
         to: [
           {
-            key_code: "h",
-            modifiers: ["right_command"],
+            key_code: "delete_or_backspace",
+            modifiers: ["left_option" ],
           },
         ],
       },
-      y: {
-        description: "Window: First Third",
+        g: {
         to: [
           {
-            key_code: "left_arrow",
-            modifiers: ["right_option", "right_control"],
+            key_code: "delete_or_backspace",
+            modifiers: ["left_command" ],
           },
         ],
       },
-      k: {
-        description: "Window: Top Half",
+        j: {
         to: [
           {
-            key_code: "up_arrow",
-            modifiers: ["right_option", "right_command"],
+            key_code: "delete_or_backspace",
           },
         ],
       },
-      j: {
-        description: "Window: Bottom Half",
-        to: [
-          {
-            key_code: "down_arrow",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      o: {
-        description: "Window: Last Third",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_option", "right_control"],
-          },
-        ],
-      },
-      h: {
-        description: "Window: Left Half",
-        to: [
-          {
-            key_code: "left_arrow",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      l: {
-        description: "Window: Right Half",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      f: {
-        description: "Window: Full Screen",
-        to: [
-          {
-            key_code: "f",
-            modifiers: ["right_option", "right_command"],
-          },
-        ],
-      },
-      u: {
-        description: "Window: Previous Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control", "right_shift"],
-          },
-        ],
-      },
-      i: {
-        description: "Window: Next Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control"],
-          },
-        ],
-      },
-      n: {
-        description: "Window: Next Window",
-        to: [
-          {
-            key_code: "grave_accent_and_tilde",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      b: {
-        description: "Window: Back",
-        to: [
-          {
-            key_code: "open_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      // Note: No literal connection. Both f and n are already taken.
-      m: {
-        description: "Window: Forward",
-        to: [
-          {
-            key_code: "close_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      d: {
-        description: "Window: Next display",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_control", "right_option", "right_command"],
-          },
-        ],
-      },
-    },
-
+},
     // s = "System"
-    s: {
-      u: {
-        to: [
-          {
-            key_code: "volume_increment",
-          },
-        ],
-      },
-      j: {
-        to: [
-          {
-            key_code: "volume_decrement",
-          },
-        ],
-      },
-      i: {
-        to: [
-          {
-            key_code: "display_brightness_increment",
-          },
-        ],
-      },
-      k: {
-        to: [
-          {
-            key_code: "display_brightness_decrement",
-          },
-        ],
-      },
-      l: {
-        to: [
-          {
-            key_code: "q",
-            modifiers: ["right_control", "right_command"],
-          },
-        ],
-      },
-      p: {
-        to: [
-          {
-            key_code: "play_or_pause",
-          },
-        ],
-      },
-      semicolon: {
-        to: [
-          {
-            key_code: "fastforward",
-          },
-        ],
-      },
-      e: {
-        to: [
-          {
-            // Emoji picker
-            key_code: "spacebar",
-            modifiers: ["right_control", "right_command"],
-          },
-        ],
-      },
-      // Turn on Elgato KeyLight
-      y: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 1, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
-          },
-        ],
-      },
-      h: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 0, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
-          },
-        ],
-      },
-    },
+    // s: {
+    //   u: {
+    //     to: [
+    //       {
+    //         key_code: "volume_increment",
+    //       },
+    //     ],
+    //   },
+    //   j: {
+    //     to: [
+    //       {
+    //         key_code: "volume_decrement",
+    //       },
+    //     ],
+    //   },
+    //   i: {
+    //     to: [
+    //       {
+    //         key_code: "display_brightness_increment",
+    //       },
+    //     ],
+    //   },
+    //   k: {
+    //     to: [
+    //       {
+    //         key_code: "display_brightness_decrement",
+    //       },
+    //     ],
+    //   },
+    //   l: {
+    //     to: [
+    //       {
+    //         key_code: "q",
+    //         modifiers: ["right_control", "right_command"],
+    //       },
+    //     ],
+    //   },
+    //   // p: {
+    //   //   to: [
+    //   //     {
+    //   //       key_code: "play_or_pause",
+    //   //     },
+    //   //   ],
+    //   // },
+    //   // semicolon: {
+    //   //   to: [
+    //   //     {
+    //   //       key_code: "fastforward",
+    //   //     },
+    //   //   ],
+    //   // },
+    //   // e: {
+    //   //   to: [
+    //   //     {
+    //   //       // Emoji picker
+    //   //       key_code: "spacebar",
+    //   //       modifiers: ["right_control", "right_command"],
+    //   //     },
+    //   //   ],
+    //   // },
+    //   // Turn on Elgato KeyLight
+    //   // y: {
+    //   //   to: [
+    //   //     {
+    //   //       shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 1, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
+    //   //     },
+    //   //   ],
+    //   // },
+    //   // h: {
+    //   //   to: [
+    //   //     {
+    //   //       shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 0, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
+    //   //     },
+    //   //   ],
+    //   // },
+    // },
 
     // v = "moVe" which isn't "m" because we want it to be on the left hand
     // so that hjkl work like they do in vim
@@ -294,30 +207,30 @@ const rules: KarabinerRules[] = [
         to: [{ key_code: "right_arrow" }],
       },
       // Magicmove via homerow.app
-      m: {
-        to: [{ key_code: "f", modifiers: ["right_control"] }],
-      },
-      // Scroll mode via homerow.app
-      s: {
-        to: [{ key_code: "j", modifiers: ["right_control"] }],
-      },
-      d: {
-        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
-      },
+      // m: {
+      //   to: [{ key_code: "f", modifiers: ["right_control"] }],
+      // },
+      // // Scroll mode via homerow.app
+      // s: {
+      //   to: [{ key_code: "j", modifiers: ["right_control"] }],
+      // },
+      // d: {
+      //   to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
+      // },
     },
 
     // c = Musi*c* which isn't "m" because we want it to be on the left hand
-    c: {
-      p: {
-        to: [{ key_code: "play_or_pause" }],
-      },
-      n: {
-        to: [{ key_code: "fastforward" }],
-      },
-      b: {
-        to: [{ key_code: "rewind" }],
-      },
-    },
+    // c: {
+    //   p: {
+    //     to: [{ key_code: "play_or_pause" }],
+    //   },
+    //   n: {
+    //     to: [{ key_code: "fastforward" }],
+    //   },
+    //   b: {
+    //     to: [{ key_code: "rewind" }],
+    //   },
+    // },
   }),
 ];
 
